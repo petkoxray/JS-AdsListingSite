2cf0c6fe754b30230b1aff46b4994955ebb5924b
Petko Kostadinov <petkoxray@gmail.com>
2017-04-04 19:25:58 +0300

    edited home index view to display only part of the ad content

--- ListingAdsApp/controllers/home.js
+++ ListingAdsApp/controllers/home.js
@@ -4,6 +4,9 @@ const Ad = mongoose.model('Ad');
 module.exports = {
   index: (req, res) => {
       Ad.find({}).limit(6).populate('author').then(ads => {
+          ads.forEach(ads => {
+              ads.content = ads.content.substr(0, 500) + '...';
+          });
           res.render('home/index', { ads: ads})
       })
   }
